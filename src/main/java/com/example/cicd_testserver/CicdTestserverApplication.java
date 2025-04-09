a15ad495c974bbbbe2235b315238c4070d614193
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-09 13:35:28 +0900

    first commit

---
+++ src/main/java/com/example/cicd_testserver/CicdTestserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd_testserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdTestserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdTestserverApplication.class, args);
+	}
+
+}
